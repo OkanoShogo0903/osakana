36357632a754f6c2c82f4b3d94483e4e38661a6c
okano_thinkpad2 <okanosyogo@gmail.com>
2020-03-01 19:15:38 +0900

    FirstCommit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+
+	"github.com/OkanoShogo0903/osakana/controller"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	speech_result := model.New()
+	router := gin.Default()
+
+	router.Use(cors.Default())
+
+	router.POST("/speech", controller.SpeechPost(speech_result))
+
+	port := ""
+	if len(os.Args) < 2 {
+		port = os.Getenv("PORT")
+	} else {
+		port = os.Args[1]
+	}
+	router.Run(":" + port)
+}
