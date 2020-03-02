8a496b4da0ec33ebba8eda6b042d73a21eef8cd2
okano_thinkpad2 <okanosyogo@gmail.com>
2020-03-02 20:29:29 +0900

    [NEW] とあるサイトを参考

--- main.go
+++ main.go
@@ -1,27 +1,43 @@
 package main
 
 import (
-	"os"
+    "os"
 
-	"github.com/OkanoShogo0903/osakana/controller"
+    "sampleapp/routers"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
+    "github.com/OkanoShogo0903/osakana/controller"
+
+    "github.com/gin-contrib/cors"
+
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
-	speech_result := model.New()
-	router := gin.Default()
+    aquarium_model := model.New()
 
-	router.Use(cors.Default())
+    router := gin.Default()
+    router.Use(cors.Default())
 
-	router.POST("/speech", controller.SpeechPost(speech_result))
+    router.LoadHTMLGlob("views/*.html")
+    router.Static("/assets", "./assets")
 
-	port := ""
-	if len(os.Args) < 2 {
-		port = os.Getenv("PORT")
-	} else {
-		port = os.Args[1]
-	}
-	router.Run(":" + port)
+    user := router.Group("/user")
+    {
+        user.POST("/signup", routers.UserSignUp)
+        user.POST("/login", routers.UserLogIn)
+    }
+
+    router.GET("/", routers.Home)
+    router.GET("/aquarium", controller.aquarium(aquarium_model))
+    router.GET("/login", routers.LogIn)
+    router.GET("/signup", routers.SignUp)
+    router.NoRoute(routers.NoRoute)
+
+    port := ""
+    if len(os.Args) < 2 {
+        port = os.Getenv("PORT")
+    } else {
+        port = os.Args[1]
+    }
+    router.Run(":" + port)
 }
